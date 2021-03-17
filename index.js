fe01a18a47d3e42bc8fa2141c8e2090d288f6cb2
fathin210 <fathin210@gmail.com>
2021-03-17 13:47:59 +0700

    add CRUD table transaksi

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 5001;
-const dbConn = require('./db.config');
+const dbConn = require('./config/db.config');
 const request = require('request')
 const api_key = 'c9bf1ee0d8f622cbc18350bbab12d1d2';
 
@@ -26,35 +26,115 @@ app.get('/movies', (req, res) => {
 })
 
 //ini route buat crud
-app.post('/add', (req, res, next) => {
-    let judul = req.body.judul;
-    let slogan = req.body.slogan;
-    let sutradara = req.body.sutradara;
+//TRANSAKSI
+//insert transaksi
+app.post('/transaksi/add', (req, res, next) => {
+    let jenis_transaksi = req.body.jenis_transaksi;
+    let lama_pinjam = req.body.lama_pinjam;
+    let biaya = req.body.biaya;
+    let denda = req.body.denda;
+    let id_member = req.body.id_member;
+    let id_film = req.body.id_film;
     let errors = false;
 
-    if (judul.length === 0 || slogan.length === 0 || sutradara.length === 0) {
+    if (jenis_transaksi.length === 0 || lama_pinjam.length === 0 || biaya.length === 0 || denda.length === 0 || id_member.length === 0 || id_film.length === 0) {
         errors = true;
-
         res.send("Ada yang kosong!")
     }
 
     else if (!errors) {
         var form_data = {
-            'judul': judul,
-            'slogan': slogan,
-            'sutradara': sutradara
+            'jenis_transaksi': jenis_transaksi,
+            'lama_pinjam': lama_pinjam,
+            'biaya': biaya,
+            'denda': denda,
+            'id_member': id_member,
+            'id_film': id_film
         }
 
-        dbConn.query('INSERT INTO film SET ?', form_data, (err, result) => {
+        dbConn.query('INSERT INTO transaksi SET ?', form_data, (err, result) => {
             if (err) {
                 res.send('error')
             } else {
-                res.send('Film successfully added!')
+                res.send('Transaksi successfully added!')
             }
         })
     }
 })
 
+//query select all transaksi
+app.get('/transaksi', (req, res) => {
+    dbConn.query('SELECT * FROM transaksi ', (err, result) => {
+        if (err) {
+            res.send('error')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+//query select id transaksi
+app.get('/transaksi/:id_transaksi', (req, res, next) => {
+    let id_transaksi = req.params.id_transaksi;
+    dbConn.query('SELECT * FROM transaksi WHERE id_transaksi = ' + id_transaksi, (err, result) => {
+        if (err) {
+            res.send('error')
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+//query update transaksi
+app.post('/transaksi/update/:id_transaksi', (req, res, next) => {
+    let id_transaksi = req.params.id_transaksi;
+    let jenis_transaksi = req.body.jenis_transaksi;
+    let lama_pinjam = req.body.lama_pinjam;
+    let biaya = req.body.biaya;
+    let denda = req.body.denda;
+    let id_member = req.body.id_member;
+    let id_film = req.body.id_film;
+    let errors = false;
+
+    if (jenis_transaksi.length === 0 || lama_pinjam.length === 0 || biaya.length === 0 || denda.length === 0 || id_member.length === 0 || id_film.length === 0) {
+        errors = true;
+        res.send("Ada yang kosong!")
+    }
+
+    else if (!errors) {
+        var form_data = {
+            'jenis_transaksi': jenis_transaksi,
+            'lama_pinjam': lama_pinjam,
+            'biaya': biaya,
+            'denda': denda,
+            'id_member': id_member,
+            'id_film': id_film
+        }
+
+        dbConn.query('UPDATE transaksi SET ? WHERE id_transaksi = ' + id_transaksi, form_data, (err, result) => {
+            if (err) {
+                res.send('error')
+            } else {
+                res.send('Transaksi successfully updated!')
+            }
+        })
+    }
+})
+
+//query delete by id
+app.post('/transaksi/delete/:id_transaksi', (req, res, next) => {
+    let id_transaksi = req.params.id_transaksi;
+    dbConn.query('DELETE FROM transaksi WHERE id_transaksi = ' + id_transaksi, (err, result) => {
+        if (err) {
+            res.send('error')
+        } else {
+            res.send('delete successfully!')
+        }
+    })
+})
+//SAMPAI SINI
+
+//
 //kalo error 404
 app.use((req, res, next) => {
     res.json({
